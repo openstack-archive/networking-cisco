74fd286249374843c842aac61e1e36082c97ff2e
Yanping Qu <yanping@cisco.com>
2015-02-12 12:37:12 -0800

    Add Cisco vendor files required by Cisco CSR service plugins.
    Add cisco_csr_rest_client which will be needed by Cisco CSR service plugins to send RESTAPI to CSR.
    Add service_helper which will be needed for Cisco service plugins interworking with Cisco cfgagent.
    Add UT test code for these two files.
    
    Change-Id: I7a05969cb81a16700712e805b3e6344e4643bba1
    Implements: blueprint fwaas-cisco

---
+++ networking_cisco/plugins/cisco/cfg_agent/device_drivers/cisco_csr_rest_client.py
@@ -0,0 +1,185 @@
+# Copyright 2015 Cisco Systems, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import time
+
+from oslo_serialization import jsonutils
+import requests
+from requests import exceptions as r_exc
+
+from neutron.i18n import _LE, _LW
+from neutron.openstack.common import log as logging
+
+
+TIMEOUT = 20.0
+
+LOG = logging.getLogger(__name__)
+HEADER_CONTENT_TYPE_JSON = {'content-type': 'application/json'}
+URL_BASE = 'https://%(host)s/api/v1/%(resource)s'
+
+
+class CsrRestClient(object):
+
+    """REST CsrRestClient for accessing the Cisco Cloud Services Router."""
+
+    def __init__(self, settings):
+        self.port = str(settings.get('protocol_port', 55443))
+        self.host = ':'.join([settings.get('rest_mgmt_ip', ''), self.port])
+        self.auth = (settings['username'], settings['password'])
+        self.token = None
+        self.status = requests.codes.OK
+        self.timeout = settings.get('timeout')
+        self.max_tries = 5
+        self.session = requests.Session()
+
+    def _response_info_for(self, response, method):
+        """Return contents or location from response.
+
+        For a POST or GET with a 200 response, the response content
+        is returned.
+
+        For a POST with a 201 response, return the header's location,
+        which contains the identifier for the created resource.
+
+        If there is an error, return the response content, so that
+        it can be used in error processing ('error-code', 'error-message',
+        and 'detail' fields).
+        """
+        if method in ('POST', 'GET') and self.status == requests.codes.OK:
+            LOG.debug('RESPONSE: %s', response.json())
+            return response.json()
+        if method == 'POST' and self.status == requests.codes.CREATED:
+            return response.headers.get('location', '')
+        if self.status >= requests.codes.BAD_REQUEST and response.content:
+            if 'error-code' in response.content:
+                content = jsonutils.loads(response.content)
+                LOG.debug("Error response content %s", content)
+                return content
+
+    def _request(self, method, url, **kwargs):
+        """Perform REST request and save response info."""
+        try:
+            LOG.debug("%(method)s: Request for %(resource)s payload: "
+                      "%(payload)s",
+                      {'method': method.upper(), 'resource': url,
+                       'payload': kwargs.get('data')})
+            start_time = time.time()
+            response = self.session.request(method, url, verify=False,
+                                            timeout=self.timeout, **kwargs)
+            LOG.debug("%(method)s Took %(time).2f seconds to process",
+                      {'method': method.upper(),
+                       'time': time.time() - start_time})
+        except (r_exc.Timeout, r_exc.SSLError) as te:
+            # Should never see SSLError, unless requests package is old (<2.0)
+            timeout_val = 0.0 if self.timeout is None else self.timeout
+            LOG.warning(_LW("%(method)s: Request timeout%(ssl)s "
+                            "(%(timeout).3f sec) for CSR(%(host)s)"),
+                        {'method': method,
+                         'timeout': timeout_val,
+                         'ssl': '(SSLError)'
+                         if isinstance(te, r_exc.SSLError) else '',
+                         'host': self.host})
+            self.status = requests.codes.REQUEST_TIMEOUT
+        except r_exc.ConnectionError:
+            LOG.exception(_LE("%(method)s: Unable to connect to "
+                              "CSR(%(host)s)"),
+                          {'method': method, 'host': self.host})
+            self.status = requests.codes.NOT_FOUND
+        except Exception as e:
+            LOG.error(_LE("%(method)s: Unexpected error for CSR (%(host)s): "
+                          "%(error)s"),
+                      {'method': method, 'host': self.host, 'error': e})
+            self.status = requests.codes.INTERNAL_SERVER_ERROR
+        else:
+            self.status = response.status_code
+            LOG.debug("%(method)s: Completed [%(status)s]",
+                      {'method': method, 'status': self.status})
+            return self._response_info_for(response, method)
+
+    def authenticate(self):
+        """Obtain a token to use for subsequent CSR REST requests.
+
+        This is called when there is no token yet, or if the token has expired
+        and attempts to use it resulted in an UNAUTHORIZED REST response.
+        """
+
+        url = URL_BASE % {'host': self.host, 'resource': 'auth/token-services'}
+        headers = {'Content-Length': '0',
+                   'Accept': 'application/json'}
+        headers.update(HEADER_CONTENT_TYPE_JSON)
+        LOG.debug("%(auth)s with CSR %(host)s",
+                  {'auth': 'Authenticating' if self.token is None
+                   else 'Reauthenticating', 'host': self.host})
+        self.token = None
+        response = self._request("POST", url, headers=headers, auth=self.auth)
+        if response:
+            self.token = response['token-id']
+            LOG.debug("Successfully authenticated with CSR %s", self.host)
+            return True
+        LOG.error(_LE("Failed authentication with CSR %(host)s [%(status)s]"),
+                  {'host': self.host, 'status': self.status})
+
+    def _do_request(self, method, resource, payload=None, more_headers=None,
+                    full_url=False):
+        """Perform a REST request to a CSR resource.
+
+        If this is the first time interacting with the CSR, a token will
+        be obtained. If the request fails, due to an expired token, the
+        token will be obtained and the request will be retried once more.
+        """
+
+        if self.token is None:
+            if not self.authenticate():
+                return
+
+        if full_url:
+            url = resource
+        else:
+            url = ('https://%(host)s/api/v1/%(resource)s' %
+                   {'host': self.host, 'resource': resource})
+        headers = {'Accept': 'application/json', 'X-auth-token': self.token}
+        if more_headers:
+            headers.update(more_headers)
+        if payload:
+            payload = jsonutils.dumps(payload)
+        response = self._request(method, url, data=payload, headers=headers)
+        if self.status == requests.codes.UNAUTHORIZED:
+            if not self.authenticate():
+                return
+            headers['X-auth-token'] = self.token
+            response = self._request(method, url, data=payload,
+                                     headers=headers)
+        if self.status != requests.codes.REQUEST_TIMEOUT:
+            return response
+        LOG.error(_LE("%(method)s: Request timeout for CSR(%(host)s)"),
+                  {'method': method, 'host': self.host})
+
+    def get_request(self, resource, full_url=False):
+        """Perform a REST GET requests for a CSR resource."""
+        return self._do_request('GET', resource, full_url=full_url)
+
+    def post_request(self, resource, payload=None):
+        """Perform a POST request to a CSR resource."""
+        return self._do_request('POST', resource, payload=payload,
+                                more_headers=HEADER_CONTENT_TYPE_JSON)
+
+    def put_request(self, resource, payload=None):
+        """Perform a PUT request to a CSR resource."""
+        return self._do_request('PUT', resource, payload=payload,
+                                more_headers=HEADER_CONTENT_TYPE_JSON)
+
+    def delete_request(self, resource):
+        """Perform a DELETE request on a CSR resource."""
+        return self._do_request('DELETE', resource,
+                                more_headers=HEADER_CONTENT_TYPE_JSON)
---
+++ networking_cisco/plugins/cisco/cfg_agent/service_helpers/service_helper.py
@@ -0,0 +1,93 @@
+# Copyright 2015 Cisco Systems, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+import Queue
+import six
+
+from neutron.openstack.common import log as logging
+LOG = logging.getLogger(__name__)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class ServiceHelperBase(object):
+
+    def __init__(self):
+        self._observers = []
+
+    def register(self, observer):
+        LOG.debug("Attaching observer: %(ob)s to subject: %(sub)s",
+                  {'ob': observer.__class__.__name__,
+                   'sub': self.__class__.__name__})
+        if observer not in self._observers:
+            self._observers.append(observer)
+        else:
+            raise ValueError(_("Observer: %(ob)s is already registered to "
+                             "subject: %(sub)s"),
+                             {'ob': observer.__class__.__name__,
+                              'sub': self.__class__.__name__})
+
+    def unregister(self, observer):
+        LOG.debug("Dettaching observer: %(ob)s from subject: %(sub)s",
+                  {'ob': observer.__class__.__name__,
+                   'sub': self.__class__.__name__})
+        if observer in self._observers:
+            self._observers.remove(observer)
+        else:
+            raise ValueError(_("Observer: %(ob)s is not attached to "
+                               "subject: %(sub)s"),
+                             {'ob': observer.__class__.__name__,
+                              'sub': self.__class__.__name__})
+
+    def notify(self, resource, **kwargs):
+        """Calls all observers attached to the given subject."""
+        LOG.debug("Notifying all observers of this subject")
+        for observer in self._observers:
+            LOG.debug("Notifying observer: %s", observer.__class__.__name__)
+            observer.update(resource, **kwargs)
+
+    def update(self, resource, **kwargs):
+        """For future support."""
+        LOG.debug("Update received")
+
+    @abc.abstractmethod
+    def process_service(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class QueueMixin(object):
+    def __init__(self):
+        super(QueueMixin, self).__init__()
+        self._queues = {}
+
+    def enqueue(self, qname, data):
+        if qname not in self._queues:
+            self._queues[qname] = Queue.Queue()
+        queue = self._queues[qname]
+        queue.put(data)
+
+    def dequeue(self, qname):
+        if qname not in self._queues:
+            raise ValueError(_("queue %s is not defined"), qname)
+        try:
+            return self._queues[qname].get(block=False)
+        except Queue.Empty:
+            return None
+
+    def qsize(self, qname):
+        """Return the approximate size of the queue."""
+        if qname in self._queues:
+            return self._queues[qname].qsize()
+        else:
+            raise ValueError(_("queue %s is not defined"), qname)
