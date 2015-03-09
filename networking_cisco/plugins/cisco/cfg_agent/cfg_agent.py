015bd8c16a723e7c7d2b3f5998cb1a3c97df973b
Yanping Qu <yanping@cisco.com>
2015-03-09 11:23:31 -0700

    Integrate Cisco FWaaS service helper with CfgAgent
    Add hooks in cfg_agent for Cisco FWaaS service helper so that
    Cisco FWaaS service helper will be initilized when ciscocfgagent
    starts and its process_services will be invoke periodically.
    
    Patch 159253 has been merged, therefore remove the line from the file.
    
    Change-Id: Ic39a7a7a3500a13c42cebaea35d057d9a59e0881
    Implements: blueprint fwaas-cisco

--- networking_cisco/plugins/cisco/cfg_agent/cfg_agent.py
+++ networking_cisco/plugins/cisco/cfg_agent/cfg_agent.py
@@ -108,6 +108,10 @@ class CiscoCfgAgent(manager.Manager):
            default='networking_cisco.plugins.cisco.cfg_agent.service_helpers'
            '.routing_svc_helper.RoutingServiceHelper',
            help=_("Path of the routing service helper class.")),
+        cfg.StrOpt('fw_svc_helper_class',
+                   default='neutron_fwaas.services.firewall.drivers.cisco'
+                           '.csr_firewall_svc_helper.CsrFirewallServiceHelper',
+                   help=_("Path of the firewall service helper class.")),
     ]
 
     def __init__(self, host, conf=None):
@@ -134,6 +138,17 @@ class CiscoCfgAgent(manager.Manager):
                      {'class': self.conf.cfg_agent.routing_svc_helper_class,
                       'reason': e})
             self.routing_service_helper = None
+
+        fw_svc_helper_class = self.conf.cfg_agent.fw_svc_helper_class
+        try:
+            self.fw_service_helper = importutils.import_object(
+                fw_svc_helper_class, host, self.conf, self)
+        except ImportError as e:
+            LOG.warn(_LW("Error in loading firewall service helper. Class "
+                       "specified is %(class)s. Reason:%(reason)s"),
+                     {'class': self.conf.cfg_agent.fw_svc_helper_class,
+                      'reason': e})
+            self.fw_service_helper = None
 
     def _start_periodic_tasks(self):
         self.loop = loopingcall.FixedIntervalLoopingCall(self.process_services)
@@ -201,6 +216,11 @@ class CiscoCfgAgent(manager.Manager):
                                                         removed_devices_info)
         else:
             LOG.warning(_LW("No routing service helper loaded"))
+
+        if self.fw_service_helper:
+            self.fw_service_helper.process_service(device_ids,
+                                                   removed_devices_info)
+
         LOG.debug("Processing services completed")
 
     def _process_backlogged_hosting_devices(self, context):
