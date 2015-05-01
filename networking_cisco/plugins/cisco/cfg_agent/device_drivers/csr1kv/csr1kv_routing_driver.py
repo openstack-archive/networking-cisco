a118483327f7a217dfedfe69da3ef91f9ec6a169
Tom Holtzen <tholtzen@cisco.com>
2015-05-01 15:07:36 -0700

    Update netorking-cisco files for due to neutrons port dictionary subnet being replaced with a subnets list.
    Change-Id: I1e873c0d7e26d0d4742008562d7a04391bb45f60
    Closes-Bug: #1450130
    (cherry picked from commit 69d7d4870740666d96539fbcfe19d2849c001a8e)

--- networking_cisco/plugins/cisco/cfg_agent/device_drivers/csr1kv/csr1kv_routing_driver.py
+++ networking_cisco/plugins/cisco/cfg_agent/device_drivers/csr1kv/csr1kv_routing_driver.py
@@ -83,13 +83,13 @@ class CSR1kvRoutingDriver(devicedriver_api.RoutingDriverBase):
 
     def external_gateway_added(self, ri, ex_gw_port):
         self._csr_create_subinterface(ri, ex_gw_port)
-        ex_gw_ip = ex_gw_port['subnet']['gateway_ip']
+        ex_gw_ip = ex_gw_port['subnets'][0]['gateway_ip']
         if ex_gw_ip:
             #Set default route via this network's gateway ip
             self._csr_add_default_route(ri, ex_gw_ip)
 
     def external_gateway_removed(self, ri, ex_gw_port):
-        ex_gw_ip = ex_gw_port['subnet']['gateway_ip']
+        ex_gw_ip = ex_gw_port['subnets'][0]['gateway_ip']
         if ex_gw_ip:
             #Remove default route via this network's gateway ip
             self._csr_remove_default_route(ri, ex_gw_ip)
