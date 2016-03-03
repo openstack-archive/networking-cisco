73b8af7526ed2ca8ea71aec85360f23639082ea4
Bob Melander <bob.melander@gmail.com>
2016-03-03 12:40:17 +0100

    Do not update host_id on ports for routers in hosting devices
    The callback from l3 config agent to the cisco l3 router plugin
    updates the host_id of the router ports to the server where the
    l3 config agent runs. This is not correct as the host_id should
    be that of the hosting device for the neutron router.
    
    This patch removes the code that updates the host_id in the
    callback. The host_id is instead already set by the plugging
    drivers (for those plugging strategies where host_id is needed).
    
    Closes bug: #1553100
    (Cherry-picked from commit: 8d3ee67d61e434c81cb4bc4b68aa0287c92edec7)
    
    Change-Id: Iaf0fefe96d3fe586bdb29b4cdfed8f0239db64d6
    (cherry picked from commit 6936273bce0f11c8232c470ec87539141f0cc9a1)

--- networking_cisco/plugins/cisco/l3/rpc/l3_router_cfg_agent_rpc_cb.py
+++ networking_cisco/plugins/cisco/l3/rpc/l3_router_cfg_agent_rpc_cb.py
@@ -18,12 +18,9 @@ from oslo_serialization import jsonutils
 import six
 
 from neutron.common import constants
-from neutron.common import exceptions
-from neutron.common import utils
 from neutron import context as neutron_context
 from neutron.db import api as db_api
 from neutron.extensions import l3
-from neutron.extensions import portbindings
 from neutron import manager
 
 LOG = logging.getLogger(__name__)
@@ -69,9 +66,6 @@ class L3RouterCfgRpcCallback(object):
                     adm_context, host, router_ids, hosting_device_ids))
         except AttributeError:
             routers = []
-        if routers and utils.is_extension_supported(
-                self._core_plugin, constants.PORT_BINDING_EXT_ALIAS):
-            self._ensure_host_set_on_ports(context, host, routers)
         LOG.debug('Routers returned to Cisco cfg agent@%(agt)s:\n %(routers)s',
                   {'agt': host, 'routers': jsonutils.dumps(routers, indent=5)})
         return routers
@@ -159,29 +153,3 @@ class L3RouterCfgRpcCallback(object):
         with context.session.begin(subtransactions=True):
             self._l3plugin.update_router_port_statuses(context, port_ids,
                                                        status)
-
-    def _ensure_host_set_on_ports(self, context, host, routers):
-        for router in routers:
-            LOG.debug("Checking router: %(id)s for host: %(host)s",
-                      {'id': router['id'], 'host': host})
-            self._ensure_host_set_on_port(context, host, router.get('gw_port'),
-                                          router['id'])
-            for interface in router.get(constants.INTERFACE_KEY, []):
-                self._ensure_host_set_on_port(context, host,
-                                              interface, router['id'])
-
-    def _ensure_host_set_on_port(self, context, host, port, router_id=None):
-        if (port and
-            (port.get('device_owner') !=
-             constants.DEVICE_OWNER_DVR_INTERFACE and
-             port.get(portbindings.HOST_ID) != host or
-             port.get(portbindings.VIF_TYPE) ==
-             portbindings.VIF_TYPE_BINDING_FAILED)):
-            try:
-                self._core_plugin.update_port(
-                    context, port['id'],
-                    {'port': {portbindings.HOST_ID: host}})
-            except exceptions.PortNotFound:
-                LOG.debug("Port %(port)s not found while updating "
-                          "agent binding for router %(router)s."
-                          % {"port": port['id'], "router": router_id})
