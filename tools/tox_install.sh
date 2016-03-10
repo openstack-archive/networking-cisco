52a4b20b11047c211834201293f11ad1c8d6ff59
Sam Betts <sam@code-smash.net>
2016-03-10 13:38:57 +0000

    Pin networking-cisco to Neutron stable/mikata
    This commit pins networking-cisco to stable/mikata now that neutron
    has opened master for Newton development. This is required until we cut
    our own stable/mikata branch.
    
    This patch also switches out the tarball caching for pip+git
    installation of neutron and neutronclient, providing the ability to set a
    default branch for neutron and the client in tox.ini and also the
    ability to override the branch settings by exporting NEUTRON_BRANCH or
    NEUTRONCLIENT_BRANCH variables in your local environment to give us
    further flexibility with the tox environment install process.
    
    Change-Id: I100a1b54021bc5885887bd0ed74d8c94a5aecbde
    Closes-Bug: #1555639

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -5,16 +5,14 @@
 set -ex
 
 ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
-
-mkdir -p .test-tars
+NEUTRON_BRANCH=${NEUTRON_BRANCH:-${DEFAULT_NEUTRON_BRANCH:-master}}
+NEUTRONCLIENT_BRANCH=${NEUTRONCLIENT_BRANCH:-${DEFAULT_NEUTRONCLIENT_BRANCH:-master}}
+UPPER_CONSTRAINTS_FILE=${UPPER_CONSTRAINTS_FILE:-unconstrained}
 
 install_cmd="pip install"
 
-CONSTRAINTS_FILE=$1
-shift
-
-if [ "$CONSTRAINTS_FILE" != "unconstrained" ]; then
-    install_cmd="$install_cmd -c$CONSTRAINTS_FILE"
+if [ "$UPPER_CONSTRAINTS_FILE" != "unconstrained" ]; then
+    install_cmd="$install_cmd -c$UPPER_CONSTRAINTS_FILE"
 fi
 
 if $(python -c "import neutronclient" 2> /dev/null); then
@@ -22,12 +20,11 @@ if $(python -c "import neutronclient" 2> /dev/null); then
 elif [ -x $ZUUL_CLONER ]; then
     # Use zuul-cloner to clone openstack/neutronclient, this will ensure the Depends-On
     # references are retrieved from zuul and rebased into the repo, then installed.
-    $ZUUL_CLONER --cache-dir /opt/git --workspace /tmp git://git.openstack.org openstack/python-neutronclient
+    $ZUUL_CLONER --cache-dir /opt/git --branch $NEUTRONCLIENT_BRANCH --workspace /tmp git://git.openstack.org openstack/python-neutronclient
     pip install /tmp/openstack/python-neutronclient
 else
-    # Download or update neutronclient-master tarball and install
-    ( cd .test-tars && curl -R -O http://tarballs.openstack.org/python-neutronclient/python-neutronclient-master.tar.gz )
-    pip install .test-tars/python-neutronclient-master.tar.gz
+    # Install neutron client from git.openstack.org
+    pip install -e git+https://git.openstack.org/openstack/python-neutronclient@$NEUTRONCLIENT_BRANCH#egg=python-neutronclient
 fi
 
 if $(python -c "import neutron" 2> /dev/null); then
@@ -35,12 +32,11 @@ if $(python -c "import neutron" 2> /dev/null); then
 elif [ -x $ZUUL_CLONER ]; then
     # Use zuul-cloner to clone openstack/neutron, this will ensure the Depends-On
     # references are retrieved from zuul and rebased into the repo, then installed.
-    $ZUUL_CLONER --cache-dir /opt/git --workspace /tmp git://git.openstack.org openstack/neutron
+    $ZUUL_CLONER --cache-dir /opt/git --branch $NEUTRON_BRANCH --workspace /tmp git://git.openstack.org openstack/neutron
     $install_cmd /tmp/openstack/neutron
 else
-    # Download or update neutron-master tarball and install
-    ( cd .test-tars && curl -R -O http://tarballs.openstack.org/neutron/neutron-master.tar.gz )
-    $install_cmd .test-tars/neutron-master.tar.gz
+    # Install neutron from git.openstack.org
+    $install_cmd -e git+https://git.openstack.org/openstack/neutron@$NEUTRON_BRANCH#egg=neutron
 fi
 
 # Install the rest of the requirements as normal
