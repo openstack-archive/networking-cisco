e0ea24d9bed2f60e9fcc57d90881c791c40829e9
Paul Michali <pc@michali.net>
2016-03-14 18:56:55 -0400

    Use constraints for networking-cisco
    Modified so that reference upper-constraints file is used for selecting
    versions (it can be overridden by an env variable, if you want to test
    other versions). Hopefully, this will be one more thing to help reduce
    breakages (as versions will be constrained to match other projects).
    
    Changed tox_install.sh to use same method to apply constraints as neutron
    does, for consistency (switched to bash too).
    
    Left xoverage, release notes, and venv targets alone, until project-config
    is updated to support constraints for these targets.
    
    Change-Id: Idb97848ddbe1ec798d3799ee0c85ad186131e30e

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Many of neutron's repos suffer from the problem of depending on neutron,
 # but it not existing on pypi. This ensures its installed into the test environment.
@@ -7,6 +7,15 @@ set -ex
 ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
 
 mkdir -p .test-tars
+
+install_cmd="pip install"
+
+CONSTRAINTS_FILE=$1
+shift
+
+if [ "$CONSTRAINTS_FILE" != "unconstrained" ]; then
+    install_cmd="$install_cmd -c$CONSTRAINTS_FILE"
+fi
 
 if $(python -c "import neutronclient" 2> /dev/null); then
     echo "Neutronclient already installed."
@@ -27,14 +36,14 @@ elif [ -x $ZUUL_CLONER ]; then
     # Use zuul-cloner to clone openstack/neutron, this will ensure the Depends-On
     # references are retrieved from zuul and rebased into the repo, then installed.
     $ZUUL_CLONER --cache-dir /opt/git --workspace /tmp git://git.openstack.org openstack/neutron
-    pip install /tmp/openstack/neutron
+    $install_cmd /tmp/openstack/neutron
 else
     # Download or update neutron-master tarball and install
     ( cd .test-tars && curl -R -O http://tarballs.openstack.org/neutron/neutron-master.tar.gz )
-    pip install .test-tars/neutron-master.tar.gz
+    $install_cmd .test-tars/neutron-master.tar.gz
 fi
 
 # Install the rest of the requirements as normal
-pip install -U $*
+$install_cmd -U $*
 
 exit $?
