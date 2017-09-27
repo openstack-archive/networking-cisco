fba22ad88e6c30eb3394bd8a16c2f5d61a4c0fcb
Sam Betts <sam@code-smash.net>
2017-09-27 17:04:33 +0100

    Take two at enabling warning-is-error correctly
    The CI doesn't use tox -e docs to build the docs, it instead calls
    python setup.py build_sphinx directly, so warning-is-error must be
    enabled in setup.cfg instead. This change makes that happen and also
    makes sure that when the docs are built on READTHEDOCS that it handles
    it correctly.
    
    Also now we're building the python module docs the docs environment
    needs to be constrained to a supported version of OpenStack neutron to
    prevent docs being broken by changes in neutron master.
    
    Change-Id: I11eb0499348a083b861a6b3bb44d9ef5387f679e

--- networking_cisco/config/base.py
+++ networking_cisco/config/base.py
@@ -24,29 +24,29 @@ class RemainderOpt(cfg.DictOpt):
     this option is included in storing them in a dictionary structure. For
     example:
 
-    A config file like this:
+    A config file like this::
 
-    [section_a]
-    host1=value1
-    host2=value2
+        [section_a]
+        host1=value1
+        host2=value2
 
-    with a RemainderOpt option defined like:
+    with a RemainderOpt option defined like::
 
-    opts = [
-        RemainderOpt('hosts')
-    ]
-    cfg.CONF.register_opts(opts, "section_a")
+        opts = [
+            RemainderOpt('hosts')
+        ]
+        cfg.CONF.register_opts(opts, "section_a")
 
-    Results in a dictionary like:
+    Results in a dictionary like::
 
-    hosts: {
-       host1: value1,
-       host2: value2
-    }
+        hosts: {
+           host1: value1,
+           host2: value2
+        }
 
-    which is accessed using the oslo config object being used, for example:
+    which is accessed using the oslo config object being used, for example::
 
-    cfg.CONF['hosts']['host1']
+        cfg.CONF['hosts']['host1']
     """
 
     def _get_from_namespace(self, namespace, group_name):
@@ -70,39 +70,39 @@ class SubsectionOpt(cfg.DictOpt):
     """An option for parsing multiple sections with sub-IDs
 
     This option allows to parse multiple defintions of the same config section
-    which have unique IDs. For example:
+    which have unique IDs. For example::
 
-    [switch:switch1]
-    address=1.1.1.1
-    password=p1
+        [switch:switch1]
+        address=1.1.1.1
+        password=p1
 
-    [switch:switch2]
-    address=2.2.2.2
-    password=p2
+        [switch:switch2]
+        address=2.2.2.2
+        password=p2
 
     Both sections are type "switch" and will provide the same configuration
     options, but one is switch1 and the other is for switch2.
 
-    Using this option this can be represented like:
+    Using this option this can be represented like::
 
-    SubsectionOpt("switch",
-                  dest="switches",
-                  subopts=[StrOpt('address')
-                           StrOpt('password')])
+        SubsectionOpt("switch",
+                      dest="switches",
+                      subopts=[StrOpt('address')
+                               StrOpt('password')])
 
     When parsed the above config file example will result in a dictionary in
-    the form:
+    the form::
 
-    switches: {
-        switch1: {
-            address: 1.1.1.1,
-            password: p1
-        },
-        switch2: {
-            address: 2.2.2.2,
-            password: p2
+        switches: {
+            switch1: {
+                address: 1.1.1.1,
+                password: p1
+            },
+            switch2: {
+                address: 2.2.2.2,
+                password: p2
+            }
         }
-    }
     """
 
     def __init__(self, name, subopts=None, **kwargs):
