eac16e28febe6206fc7e7a8a368271cfa70f7a1f
Sam Betts <sam@code-smash.net>
2017-10-25 13:42:56 +0100

    Optimise the tox_install script
    This change prevents unnessesary reinstalls of the same version of
    neutron when running the openstack CI which should speed things up.
    
    Change-Id: I8ccd2fedb13f6a4c99d03dc7b3822ef5d8959cee

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -19,28 +19,28 @@ fi
 install_cmd="pip install -c$UPPER_CONSTRAINTS_FILE"
 
 
-if [ -d "/home/zuul/src/git.openstack.org/openstack/python-neutronclient" ]; then
-    (cd /home/zuul/src/git.openstack.org/openstack/python-neutronclient && \
-     git checkout $NEUTRONCLIENT_BRANCH && \
-     pip install -e .)
-fi
-
 if $(python -c "import neutronclient" 2> /dev/null); then
     echo "Neutronclient already installed."
+elif [ -d "/home/zuul/src/git.openstack.org/openstack/python-neutronclient" ]; then
+    # We're in a ZuulV3 environment then we needs to setup the source locations
+    # to the right branch the tox-install-siblings task will handle the rest
+    (cd /home/zuul/src/git.openstack.org/openstack/python-neutronclient && \
+     git checkout $NEUTRONCLIENT_BRANCH )
+    pip install -e /home/zuul/src/git.openstack.org/openstack/python-neutronclient
 else
     # Install neutron client from git.openstack.org
     # Dont use upper contraints here because python-neutronclient is in upperconstraints
     pip install -e git+https://git.openstack.org/openstack/python-neutronclient@$NEUTRONCLIENT_BRANCH#egg=python-neutronclient
 fi
 
-if [ -d "/home/zuul/src/git.openstack.org/openstack/neutron" ]; then
-    (cd /home/zuul/src/git.openstack.org/openstack/neutron && \
-     git checkout $NEUTRON_BRANCH && \
-     $install_cmd -e .)
-fi
-
 if $(python -c "import neutron" 2> /dev/null); then
     echo "Neutron already installed."
+elif [ -d "/home/zuul/src/git.openstack.org/openstack/neutron" ]; then
+    # We're in a ZuulV3 environment then we needs to setup the source locations
+    # to the right branch the tox-install-siblings task will handle the rest
+    (cd /home/zuul/src/git.openstack.org/openstack/neutron && \
+     git checkout $NEUTRON_BRANCH)
+    $install_cmd -e /home/zuul/src/git.openstack.org/openstack/neutron
 else
     # Install neutron from git.openstack.org
     $install_cmd -e git+https://git.openstack.org/openstack/neutron@$NEUTRON_BRANCH#egg=neutron
